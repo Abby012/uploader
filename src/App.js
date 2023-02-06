00c99ec6d4251a6dbd8ef782a56cd9312ccf83c2
Abby012 <gaoabigail0@gmail.com>
2023-02-05 21:14:24 -0500

    initialize

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    selectedFile: null,
+    fileUploadedSuccessfully: false
+  }
+
+onFileChange = event => {
+  this.setState({selectedFile: event.target.files[0]});
+}
+
+onFileUpload = () => {
+  const formData = new FormData()
+  formData.append(
+    "demo file",
+    this.state.selectedFile,
+    this.state.selectedFile.name
+  )
+  //call api
+  console.log(formData);
+  this.setState({selectedFile: null});
+  this.setState({fileUploadedSuccessfully: true});
+}
+
+fileData = () => {
+  console.log(this.state.selectedFile)
+  if (this.state.selectedFile) {
+    return (
+      <div>
+        <h2>File Details:</h2>
+        <p>File Name: {this.state.selectedFile.name}</p>
+        <p>File Type: {this.state.selectedFile.type}</p>
+        <p>Last Modified: {" "}
+          {this.state.selectedFile.lastModifiedDate.toDateString()}
+        </p>
+      </div>
+    );
+  } else if (this.state.fileUploadedSuccessfully) {
+    return (
+      <div>
+        <br />
+        <h4>Your file has been successfully uploaded</h4>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <br />
+        <h4>Choose a file and then press the Upload button</h4>
+      </div>
+    )
+  }
+}
+
+  render() {
+    return (
+      <div className="container">
+        <h2>Abby File Upload System</h2>
+        <h3>File Upload with React and a Serverless API!</h3>
+        <div>
+          <input type='file' onChange={this.onFileChange} />
+          <button onClick={this.onFileUpload}>
+            Upload
+          </button>
+        </div>
+        {this.fileData()}
+      </div>
+    )
+  }
+}
+
+export default App;
